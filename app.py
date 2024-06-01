f9b3882934103b43b1f61ec8989719bf0fc688f2
vplapps <68125352+vplapps@users.noreply.github.com>
2024-06-01 17:02:52 +0300

    Add files via upload

--- app.py
+++ app.py
@@ -4,21 +4,21 @@ import os
 
 app = Flask(__name__)
 
-# Налаштування бази даних Amazon RDS
-db_identifier = 'new'
-username = os.getenv('DB_USERNAME', 'mycmdxhtqd')
+# Отримання значень змінних середовища
+username = os.getenv('DB_USERNAME', 'plysiuk')
 password = os.getenv('DB_PASSWORD', '37Zavama')
-host = os.getenv('DB_HOST', 'plysiuk-server.postgres.database.azure.com')
+host = os.getenv('DB_HOST', 'plysiukdb.postgres.database.azure.com')
 port = os.getenv('DB_PORT', '5432')
 dbname = os.getenv('DB_NAME', 'postgres')
 
 
 # Підключення до бази даних PostgreSQL
-app.config['SQLALCHEMY_DATABASE_URI'] = f'postgresql://{username}:{password}@{host}:{port}'
+app.config['SQLALCHEMY_DATABASE_URI'] = (
+    f"postgresql+psycopg2://{username}:{password}@{host}:{port}/{dbname}"
+)
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
-
 
 # Модель користувача
 class User(db.Model):
@@ -30,52 +30,38 @@ class User(db.Model):
         self.email = email
         self.password = password
 
-
 @app.route('/')
 def home():
     return render_template('home.html')
-
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     if request.method == 'POST':
         email = request.form['email']
         password = request.form['password']
-
-        # Перевірка, чи користувач з таким email вже існує
         existing_user = User.query.filter_by(email=email).first()
         if existing_user:
             return render_template('register.html', error="User already exists with that email.")
-
-        # Додавання нового користувача до бази даних
         new_user = User(email=email, password=password)
         db.session.add(new_user)
         db.session.commit()
-
         return render_template('register.html', message="Registration successful. You can now login.")
-
     return render_template('register.html')
-
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
         email = request.form['email']
         password = request.form['password']
-
-        # Перевірка користувача в базі даних
         user = User.query.filter_by(email=email, password=password).first()
         if user:
-            return render_template('login.html', message="Logged in successfully!.")
+            return render_template('login.html', message="Logged in successfully!")
         else:
             return render_template('login.html', error="Invalid credentials. Please try again.")
-
     return render_template('login.html')
-
 
 @app.route('/users')
 def users():
-    # Отримання всіх користувачів з бази даних
     users = User.query.all()
     return render_template('users.html', users=users)
 
@@ -85,7 +71,6 @@ def delete_user(user_id):
     db.session.delete(user)
     db.session.commit()
     return redirect(url_for('users'))
-
 
 if __name__ == '__main__':
     with app.app_context():
